93f9f7a8452cab5b2adff7c4cb7f5b06e10482da
thaivm-0609 <vuongthai95@gmail.com>
2025-03-13 11:20:22 +0700

    feat: init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
+@Component({ //meta-data
+  selector: 'app-root', //cách gọi component trong trang khác
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'wd19322_angular';
+
+export class AppComponent { //khai báo biến/hàm sẽ sử dụng trong component
+  title = 'thaivm2';
+  listStudents = [
+    {
+      maSv: 'PH12345',
+      ten: 'thaivm2',
+      mail: 'thaivm2@fpt.edu.vn',
+      nganh: 'Web',
+      anh: 'https://picsum.photos/200'
+    },
+    {
+      maSv: 'PH12346',
+      ten: 'thaivm3',
+      mail: 'thaivm3@fpt.edu.vn',
+      nganh: 'Mobile',
+      anh: 'https://picsum.photos/200'
+    },
+    {
+      maSv: 'PH12347',
+      ten: 'thaivm4',
+      mail: 'thaivm4@fpt.edu.vn',
+      nganh: 'Marketing',
+      anh: 'https://picsum.photos/200'
+    }
+  ]
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { CreateComponent } from './create/create.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'list',
+        component: ListComponent,
+    },
+    {
+        path: 'create',
+        component: CreateComponent,
+    }
+];
