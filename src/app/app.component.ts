eb8596c3c7975237003463832ff974e52816ff3a
thaivm-0609 <vuongthai95@gmail.com>
2025-03-15 11:15:05 +0700

    feat: data-binding directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,50 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; //sử dụng directives(ngFor/ngIf)
+import { FormsModule } from '@angular/forms'; //sử dụng [(ngModel)]
 
 @Component({ //meta-data
-  selector: 'app-root', //cách gọi component trong trang khác
-  imports: [RouterOutlet],
+  selector: 'app-root', //cách gọi component trong component khác
+  imports: [
+    RouterOutlet, 
+    CommonModule, 
+    FormsModule
+  ], //package/module cần import/sử dụng trong component
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent { //khai báo biến/hàm sẽ sử dụng trong component
   title = 'thaivm2';
+  count:number = 0;
+  countClick():number {
+    // this.count++; //cộng thêm 1 đơn vị
+    return this.count+=2; //cộng thêm bất kỳ += số;
+  }
   listStudents = [
     {
       maSv: 'PH12345',
       ten: 'thaivm2',
       mail: 'thaivm2@fpt.edu.vn',
       nganh: 'Web',
-      anh: 'https://picsum.photos/200'
+      anh: 'https://picsum.photos/200',
+      thi: 3
     },
     {
       maSv: 'PH12346',
       ten: 'thaivm3',
       mail: 'thaivm3@fpt.edu.vn',
       nganh: 'Mobile',
-      anh: 'https://picsum.photos/200'
+      anh: 'https://picsum.photos/200',
+      thi: 5
     },
     {
       maSv: 'PH12347',
       ten: 'thaivm4',
       mail: 'thaivm4@fpt.edu.vn',
       nganh: 'Marketing',
-      anh: 'https://picsum.photos/200'
+      anh: 'https://picsum.photos/200',
+      thi: 10
     }
   ]
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { ListComponent } from './list/list.component';
 import { CreateComponent } from './create/create.component';
 
 export const routes: Routes = [
-    {
-        path: 'list',
-        component: ListComponent,
-    },
-    {
-        path: 'create',
-        component: CreateComponent,
-    }
+    // {
+    //     path: 'list',
+    //     component: ListComponent,
+    // },
+    // {
+    //     path: 'create',
+    //     component: CreateComponent,
+    // }
 ];
