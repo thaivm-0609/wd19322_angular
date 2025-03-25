b83fb5344edfa45dacfd8e7db61a6fa9614edd81
thaivm-0609 <vuongthai95@gmail.com>
2025-03-25 09:52:10 +0700

    feat: edit

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-edit',
@@ -11,7 +11,8 @@ import { FormsModule } from '@angular/forms';
 export class EditComponent {
   constructor(
     private actRoute:ActivatedRoute,
-    private api:HttpClient
+    private api:HttpClient,
+    private router:Router
   ) {}
 
   apiUrl:string = 'http://localhost:3000/students';
@@ -20,12 +21,10 @@ export class EditComponent {
 
   ngOnInit():void {
     this.id = this.actRoute.snapshot.params['id']; //gán lại giá trị id bằng tham số truyền trên URL
-    console.log(this.id);
     this.getDetail();
   }
 
-  getDetail():void {
-    console.log(this.id);
+  getDetail():void { //lấy thông tin chi tiết -> đổ ra form edit
     this.api.get(`${this.apiUrl}/${this.id}`).subscribe(res => {
       if (res) {
         this.oldStudent = res;
@@ -33,7 +32,12 @@ export class EditComponent {
     })
   }
 
-  onEdit(data:any):void {
-
+  onEdit(data:any):void { //cập nhật dữ liệu lên json-server
+    this.api.put(`${this.apiUrl}/${this.id}`,data).subscribe(res => {
+      if (res) {
+        alert('Cập nhật thành công');
+        this.router.navigate(['']);
+      }
+    })
   }
 }
